0779add182111a7da30fdd2937e6702378479ebc
Admin <Admin@ADMIN-PC>
2025-11-25 11:37:59 +0700

    buoi 5

--- nhapmonlaptrinh/buoi5/buoi5.cpp
+++ nhapmonlaptrinh/buoi5/buoi5.cpp
@@ -2,19 +2,36 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   /* cout << " nhap so: ";
+    int n;
+    cin >> n;
+    if (n%2 == 0) {
+        cout << " so " << n << " la so chan " << endl;
+    }
+    else {
+        cout << "so " << n << " la so le" << endl;*/
+
+    }
+
+
+
+ cout << " nhap nam: ";
+int year;
+cin >> year;
+if ((year % 4 == 0 && %100 != 0) || (year % 400 == 0)) {
+    cout << year << " la nam nhuan" << endl;
+}
+else {
+    cout << year << " khong phai nam nhuan" << endl;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+
